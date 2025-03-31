1dec4ae913f482ce89d06b99979363ea5add2101
AlanDReeves <alandanielreeves@gmail.com>
2025-03-30 19:38:06 -1000

    contacts table finished in 29 minutes.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,63 +1,34 @@
 import { getServerSession } from 'next-auth';
-import { Col, Container, Row, Table } from 'react-bootstrap';
-import StuffItemAdmin from '@/components/StuffItemAdmin';
+import { Col, Container, Row } from 'react-bootstrap';
 import { prisma } from '@/lib/prisma';
 import { adminProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
+// eslint-disable-next-line @typescript-eslint/quotes
+import { Contact } from "@/lib/validationSchemas";
+import AdminContactCard from '@/components/AdminContactCard';
 
 const AdminPage = async () => {
+  const contacts: Contact[] = await prisma.contact.findMany({});
   const session = await getServerSession(authOptions);
   adminProtectedPage(
     session as {
       user: { email: string; id: string; randomKey: string };
     } | null,
   );
-  const stuff = await prisma.stuff.findMany({});
-  const users = await prisma.user.findMany({});
 
   return (
     <main>
       <Container id="list" fluid className="py-3">
         <Row>
           <Col>
-            <h1>List Stuff Admin</h1>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Quantity</th>
-                  <th>Condition</th>
-                  <th>Owner</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {stuff.map((item) => (
-                  <StuffItemAdmin key={item.id} {...item} />
-                ))}
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h1>List Users Admin</h1>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Email</th>
-                  <th>Role</th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map((user) => (
-                  <tr key={user.id}>
-                    <td>{user.email}</td>
-                    <td>{user.role}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
+            <h1>List Contacts Admin</h1>
+            <Row xs={1} md={2} lg={3} className="g-4">
+              {contacts.map((contact) => (
+                <Col key={contact.firstName + contact.lastName}>
+                  <AdminContactCard {...contact} />
+                </Col>
+              ))}
+            </Row>
           </Col>
         </Row>
       </Container>
